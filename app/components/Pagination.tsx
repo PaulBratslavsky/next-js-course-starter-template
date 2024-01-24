f97524d13863cf48256aa7b21f258a72cf63ec86
Paul Bratslavsky <paulbratslavsky@Pauls-MacBook-Pro.local>
2024-01-24 01:08:53 -0600

    added pagination and search

--- app/components/Pagination.tsx
+++ app/components/Pagination.tsx
@@ -1,10 +1,66 @@
-export default function Pagination() {
+"use client";
+import { useRouter } from "next/navigation";
+import { usePathname, useSearchParams } from "next/navigation";
+
+interface PaginationProps {
+  currentPage: number;
+  totalPages: number;
+}
+
+interface PaginationArrowProps {
+  direction: "left" | "right";
+  href: string;
+  isDisabled: boolean;
+}
+
+function PaginationArrow({
+  direction,
+  href,
+  isDisabled,
+}: PaginationArrowProps) {
+  const router = useRouter();
+  const isLeft = direction === "left";
+  const disabledClassName = isDisabled ? "opacity-50 cursor-not-allowed" : "";
+
+  return (
+    <button
+      onClick={() => router.push(href)}
+      className={`join-item btn ${disabledClassName}`}
+      aria-disabled={isDisabled}
+      disabled={isDisabled}
+    >
+      {isLeft ? "«" : "»"}
+    </button>
+  );
+}
+
+export default function Pagination({ totalPages = 5 }: PaginationProps) {
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+  const currentPage = Number(searchParams.get("page")) || 1;
+
+  const createPageURL = (pageNumber: number | string) => {
+    const params = new URLSearchParams(searchParams);
+    params.set("page", pageNumber.toString());
+    return `${pathname}?${params.toString()}`;
+  };
+
   return (
     <div className="w-64 mx-auto my-6">
       <div className="join">
-        <button className="join-item btn">«</button>
-        <button className="join-item btn">Page {1}</button>
-        <button className="join-item btn">»</button>
+        <PaginationArrow
+          direction="left"
+          href={createPageURL(currentPage - 1)}
+          isDisabled={currentPage <= 1}
+        />
+        <div className="join-item flex items-center px-4 bg-base-200">
+          <span>Page {currentPage}</span>
+        </div>
+        <PaginationArrow
+          direction="right"
+          href={createPageURL(currentPage + 1)}
+          isDisabled={currentPage >= totalPages}
+        />
       </div>
     </div>
   );
--- app/components/Search.tsx
+++ app/components/Search.tsx
@@ -1,9 +1,33 @@
+"use client";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import { useDebouncedCallback } from "use-debounce";
+
 export default function Search() {
+  const searchParams = useSearchParams();
+  const { replace } = useRouter();
+  const pathname = usePathname();
+
+  const handleSearch = useDebouncedCallback((term: string) => {
+    console.log(`Searching... ${term}`);
+    const params = new URLSearchParams(searchParams);
+    params.set("page", "1");
+
+    if (term) {
+      params.set("query", term);
+    } else {
+      params.delete("query");
+    }
+
+    replace(`${pathname}?${params.toString()}`);
+  }, 300);
+
   return (
     <div className="form-control mt-6">
       <input
         type="text"
         placeholder="Search"
+        onChange={(e) => handleSearch(e.target.value)}
+        defaultValue={searchParams.get("query")?.toString()}
         className="input input-primary input-bordered w-full md:w-auto"
       />
     </div>
